ce48d57578b03f4b1c470ed9f1d8b72a33e1a00b
bukiz-cloud <adeniyibukunmiadetunji@gmail.com>
2022-06-21 02:19:52 -0700

    adding of the cart page to the folder

---
+++ src/components/OrderUnsuccessful.js
@@ -0,0 +1,16 @@
+// import './css/OrderUnsuccessful.css';
+
+// function OrderUnsuccessful() {
+//     return (
+//         <div className='unsuccessful-order' >
+//             <img className='unsuccessful-img' src="./assets/error.png" alt="successful" />
+
+//             <div className='unsuccessful-text' >
+//             <p>Order Unsuccessful</p>
+//             <h5>Something is wrong with your order.</h5>
+//             </div>            
+//         </div>
+//     )
+// }
+
+// export default OrderUnsuccessful
